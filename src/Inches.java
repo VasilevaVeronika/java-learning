c26357832f760a11030faf0ba70b7d2fb9bec2dd
VasilevaVeronika <Sv670911@gmail.com>
2021-09-21 21:49:41 +0600

    Create Inches.java
    расчёт числа в куб.дюймов в кубе

---
+++ src/Inches.java
@@ -0,0 +1,9 @@
+public class Inches {
+    public static void main(String args[]){
+        long ci;
+        long im;
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("В одной  кубической миле содержится" + ci + "кубических дюймов");
+    }
+}
