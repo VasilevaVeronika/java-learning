f89d8f817358b8c2a821b49f65aed80478c5a97a
VasilevaVeronika <Sv670911@gmail.com>
2021-09-27 20:08:11 +0600

    Create BoolDemo.java

---
+++ src/BoolDemo.java
@@ -0,0 +1,13 @@
+public class BoolDemo {
+    public static void main(String[] args){
+            boolean b;
+
+            b = false;
+            System.out.println("Значение b: " + b);
+            b = true;
+            System.out.println("Значение b: " + b);
+
+            b = false;
+        System.out.println("Результат сравнения 10 > 9 " + (10 > 9) );
+    }
+}
