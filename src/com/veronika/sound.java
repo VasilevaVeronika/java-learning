9b75a51322341edeeb22093d3fe8fd794cfcde7f
VasilevaVeronika <Sv670911@gmail.com>
2021-09-28 19:50:53 +0600

    Create sound.java

---
+++ src/com/veronika/sound.java
@@ -0,0 +1,11 @@
+package com.veronika;
+
+public class sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 1100 * 7.2;
+
+        System.out.println("Расстояние до менста вспышки молнии " + " составляет " + dist + " футов");
+    }
+}
