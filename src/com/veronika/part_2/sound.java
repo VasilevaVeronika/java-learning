c9c0c312c0de36e0fe814ff9144b1bbf173699ac
VasilevaVeronika <Sv670911@gmail.com>
2021-09-30 22:11:32 +0600

    Update sound.java

--- src/com/veronika/part_2/sound.java
+++ src/com/veronika/part_2/sound.java
@@ -1,16 +1,17 @@
 package com.veronika.part_2;
 
 // Упражнение 2.1
+
 class Sound {
     public static void main(String[] args) {
 
-        double dist;
+        double dist, distanceInMeters;
 
         dist = 1100 * 7.2;
 
-        System.out.println("Расстояние до места вспышки молнии составляет " + dist + " футов");
+        distanceInMeters = 330 * 7.2;
 
-        double distanceInMeters = 330 * 7.2;
+        System.out.println("Расстояние до места вспышки молнии составляет " + dist + " футов");
 
         System.out.println("Расстояние до места вспышки молнии составляет " + distanceInMeters + " метров");
     }
