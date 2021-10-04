6c545652dc8c1e4b2fb1ab3eaa5a5be2331eac30
VasilevaVeronika <Sv670911@gmail.com>
2021-10-04 18:36:55 +0600

    Update str_demo.java

--- src/com/veronika/part_2/str_demo.java
+++ src/com/veronika/part_2/str_demo.java
@@ -2,8 +2,9 @@ package com.veronika.part_2;
 
 public class str_demo {
     public static void main(String[] args) {
-        System.out.println("Первая строка\nВторая строка");
+        System.out.println("Первая строка\nВторая строка\nТретья строка");
         System.out.println("A\tB\tC");
         System.out.println("D\tE\tF");
+        System.out.println("I\tLove\tYou");
     }
 }
