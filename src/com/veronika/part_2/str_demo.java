dcc95a649b0afbaa77866fc24a026ed04ac3f29b
VasilevaVeronika <Sv670911@gmail.com>
2021-10-04 18:06:11 +0600

    Create str_demo.java

---
+++ src/com/veronika/part_2/str_demo.java
@@ -0,0 +1,9 @@
+package com.veronika.part_2;
+
+public class str_demo {
+    public static void main(String[] args) {
+        System.out.println("Первая строка\nВторая строка");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
