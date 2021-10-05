69ac23361fe42fafcfe5d6f134e1ef1cbdd586df
VasilevaVeronika <Sv670911@gmail.com>
2021-10-05 19:08:28 +0600

    Update DynInit.java

--- src/com/veronika/part_2/DynInit.java
+++ src/com/veronika/part_2/DynInit.java
@@ -3,7 +3,8 @@ package com.veronika.part_2;
 public class DynInit {
     public static void main(String[] args) {
 
-        double radius = 4, height = 5;
+        double radius = 4;
+        double height = 5;
         double volume = 3.1416 * radius * radius * height;
 
         System.out.println("Объём: " + volume);
