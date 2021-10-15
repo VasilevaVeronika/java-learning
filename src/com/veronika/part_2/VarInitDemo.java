a0dd620c73c06bd173ba2b996623e247017a432c
VasilevaVeronika <Sv670911@gmail.com>
2021-10-15 09:11:40 +0600

    Update VarInitDemo.java

--- src/com/veronika/part_2/VarInitDemo.java
+++ src/com/veronika/part_2/VarInitDemo.java
@@ -1,10 +1,10 @@
 package com.veronika.part_2;
 
 public class VarInitDemo {
-    public static void main(String[] args) {
 
-        int x;
-        for (x = 0; x < 3; x++) {
+    public static void main(String[] args) {
+        
+        for(int x = 0; x < 3; x++){
 
             int y = -1;
             System.out.println("y:" + y);
