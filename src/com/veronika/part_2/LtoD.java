27069ec526df611235af6abc7217c66e20f060d2
VasilevaVeronika <Sv670911@gmail.com>
2021-10-20 22:58:29 +0600

    Create LtoD.java

---
+++ src/com/veronika/part_2/LtoD.java
@@ -0,0 +1,14 @@
+package com.veronika.part_2;
+
+public class LtoD {
+
+    public static void main(String[] args) {
+        long L;
+        double D;
+
+        L = 100123285L;
+        D = L;
+
+        System.out.println("L и D : " + L + " " + D);
+    }
+}
