c1c4275df05bd221d4fc9a405b935c8e1e614d57
VasilevaVeronika <Sv670911@gmail.com>
2021-10-20 17:41:32 +0600

    Create SCops.java

---
+++ src/com/veronika/part_2/SCops.java
@@ -0,0 +1,19 @@
+package com.veronika.part_2;
+
+public class SCops {
+    public static void main(String[] args){
+        int n, d, q;
+
+        n = 10;
+        d = 2;
+        if (d != 0 && (n % d) == 0);
+        System.out.println(d + " является делителем " + n);
+
+        d = 0;
+        if(d != 0 && (n % d) == 0);
+        System.out.println(d + " является делителем" + n);
+
+        if (d != 0 & (n % d) == 0);
+        System.out.println(d + " является делителем " + n);
+    }
+}
