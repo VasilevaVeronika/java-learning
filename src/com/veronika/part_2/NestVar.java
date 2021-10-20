ed867e91704f3e77c10cf4a02dc26cd6467e51d4
VasilevaVeronika <Sv670911@gmail.com>
2021-10-20 14:46:56 +0600

    Update NestVar.java

--- src/com/veronika/part_2/NestVar.java
+++ src/com/veronika/part_2/NestVar.java
@@ -11,11 +11,7 @@ public class NestVar {
 
             for (count = 0; count < 2; count++) {
                 System.out.println("В этой программе есть ошибка");
-
             }
-
         }
-
     }
-    
 }
