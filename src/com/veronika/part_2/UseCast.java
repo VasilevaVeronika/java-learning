9ae4a2f0fb605000bb5a1ec61ca870efe3f3406a
VasilevaVeronika <Sv670911@gmail.com>
2021-10-24 21:03:01 +0600

    Create UseCast.java

---
+++ src/com/veronika/part_2/UseCast.java
@@ -0,0 +1,14 @@
+package com.veronika.part_2;
+
+public class UseCast {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 5; i++) {
+
+            System.out.println(i + " / 3" + i / 3);
+            System.out.println(i + " / 3 с дробной частью: " + (double) i / 3);
+            System.out.println();
+        }
+    }
+}
