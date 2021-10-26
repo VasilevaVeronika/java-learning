bb1fa393ad7834726a4bd10541264759cd491e4a
VasilevaVeronika <Sv670911@gmail.com>
2021-10-26 16:08:01 +0600

    Update UseCast.java

--- src/com/veronika/part_2/UseCast.java
+++ src/com/veronika/part_2/UseCast.java
@@ -3,10 +3,10 @@ package com.veronika.part_2;
 public class UseCast {
 
     public static void main(String[] args) {
-        int i;
-        for (i = 0; i < 5; i++) {
 
-            System.out.println(i + " / 3" + i / 3);
+        for(int i = 0; i < 5; i++) {
+
+            System.out.println(i + " / 3 = " + i / 3);
             System.out.println(i + " / 3 с дробной частью: " + (double) i / 3);
             System.out.println();
         }
