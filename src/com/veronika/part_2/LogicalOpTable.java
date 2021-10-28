107436b3e0b32b9f4a1bef2200667376d227f845
VasilevaVeronika <Sv670911@gmail.com>
2021-10-28 13:12:26 +0600

    Update LogicalOpTable.java

--- src/com/veronika/part_2/LogicalOpTable.java
+++ src/com/veronika/part_2/LogicalOpTable.java
@@ -2,39 +2,40 @@ package com.veronika.part_2;
 
 public class LogicalOpTable {
 
-
     public static void main(String[] args) {
         boolean p, q;
+
         System.out.println("P\tQ\tAND\tQR\tXOR\tNOT");
 
         p = true;
+
         q = true;
         System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p & q || q) + "\t");
         System.out.println((p & q) + "\t" + (!p));
 
         p = true;
         q = false;
         System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p & q || q) + "\t");
         System.out.println((p & q) + "\t" + (!p));
 
         p = true;
         q = false;
         System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p & q || q) + ("\t"));
         System.out.println((p & q) + "\t" + (!p));
 
         p = false;
         q = true;
         System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p & q || q) + ("\t"));
         System.out.println((p & q) + "\t" + (!p));
 
         p = false;
-        p = true;
+        q = true;
         System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p & q || q) + "\t");
         System.out.println((p & q) + "\t" + (!p));
     }
 }
