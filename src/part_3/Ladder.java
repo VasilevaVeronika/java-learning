252a38775fb7ae288ede151bb8abaaf7286fd8f5
VasilevaVeronika <Sv670911@gmail.com>
2022-01-05 18:48:51 +0600

    Create Ladder.java

---
+++ src/part_3/Ladder.java
@@ -0,0 +1,20 @@
+package part_3;
+
+public class Ladder {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 6; x++) {
+            if (x == 1)
+                System.out.println("x равно 1");
+            else if (x == 2)
+                System.out.println("x равно 2");
+            else if (x == 3)
+                System.out.println("x равно 3");
+            else if (x == 4)
+                System.out.println("x равно 4");
+            else
+                System.out.println("Значение x находится вне диапазогна  1-4");
+        }
+    }
+}
