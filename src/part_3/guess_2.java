1d76d405edf5e90308f94c3ab68907d2080df502
VasilevaVeronika <Sv670911@gmail.com>
2022-01-05 17:48:45 +0600

    Create guess_2.java

---
+++ src/part_3/guess_2.java
@@ -0,0 +1,16 @@
+package part_3;
+
+public class guess_2 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char ch, answer = 'K';
+
+        System.out.println("Задумана буква из диапазона A-Z.");
+        System.out.println("Попытайтесь её угадать: ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer) System.out.println("** Вы угадали! **");
+        else System.out.println("...Извините, вы не угадали.");
+    }
+}
