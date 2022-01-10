c90e09f1e45cd74e41705306059b664dec9313cd
VasilevaVeronika <Sv670911@gmail.com>
2022-01-10 20:35:42 +0600

    Create Comma.java

---
+++ src/com/veronika/part_3/Comma.java
@@ -0,0 +1,9 @@
+package com.veronika.part_3;
+
+public class Comma {
+    public static void main(String[] args) {
+
+        for (int i = 0, j = 10; i < j; i++, j--)
+            System.out.println("i и j: " + i + " " + j);
+    }
+}
