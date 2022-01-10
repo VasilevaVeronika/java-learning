df8680c73ad6070c7b1a518ca357fb91f3b5c7dd
VasilevaVeronika <Sv670911@gmail.com>
2022-01-10 21:56:20 +0600

    Create Empty.java

---
+++ src/com/veronika/part_3/Empty.java
@@ -0,0 +1,12 @@
+package com.veronika.part_3;
+
+public class Empty {
+    public static void main(String[] args){
+        int i;
+
+        for(i = 0; i < 10;) {
+            System.out.println("Проход #" + i);
+            i++;
+        }
+    }
+}
