3f60570ea20bce4ce5a12b9d5a4c842d5c0a1dfb
VasilevaVeronika <Sv670911@gmail.com>
2022-01-11 20:50:21 +0600

    Create WhileDemo.java

---
+++ src/com/veronika/part_3/WhileDemo.java
@@ -0,0 +1,12 @@
+package com.veronika.part_3;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'a';
+        while (ch < -'z') ;
+        System.out.print(ch);
+        ch++;
+    }
+}
