692a08e975162477acb6b6ab4ba5be947e950d03
VasilevaVeronika <Sv670911@gmail.com>
2022-01-11 20:19:37 +0600

    Create ForVar.java

---
+++ src/com/veronika/part_3/ForVar.java
@@ -0,0 +1,15 @@
+package com.veronika.part_3;
+
+public class ForVar {
+    public static void main(String[] args){
+        int sum = 0;
+        int fact = 1;
+
+        for(int i = 0; i <= 5; i++){
+            sum += i ;
+            fact *= i;
+        }
+        System.out.println("Cумма: " + sum);
+        System.out.println("Факториал: + fact");
+    }
+}
