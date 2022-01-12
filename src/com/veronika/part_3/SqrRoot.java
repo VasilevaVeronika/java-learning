e91c9cf36facc0daa1232dae7fe9e421d4b26655
VasilevaVeronika <Sv670911@gmail.com>
2022-01-12 18:45:11 +0600

    Update SqrRoot.java

--- src/com/veronika/part_3/SqrRoot.java
+++ src/com/veronika/part_3/SqrRoot.java
@@ -2,15 +2,16 @@ package com.veronika.part_3;
 
 public class SqrRoot {
     public static void main(String[] args) {
-        
-        double num, sroot = 0, rerr;
-        
+
+        double num, sroot, rerr;
+
         for (num = 1.0; num < 100.0; num++) {
             sroot = Math.sqrt(num);
             System.out.println("Корень квадратный из " + num + " равен " + sroot);
+
+            rerr = num - (sroot * sroot);
+            System.out.println("Ошибка округления: " + rerr);
+            System.out.println();
         }
-        rerr = num - (sroot * sroot);
-        System.out.println("Ошибка округления: " + rerr);
-        System.out.println();
     }
 }
