054184f123686c27087e1c14bf0ef07ee71c812e
VasilevaVeronika <Sv670911@gmail.com>
2022-01-20 20:29:52 +0600

    Create Break3.java

---
+++ src/com/veronika/part_3/Break3.java
@@ -0,0 +1,16 @@
+package com.veronika.part_3;
+
+public class Break3 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+
+        char ch;
+
+        for (; ; ) {
+            ch = (char) System.in.read();
+
+            if (ch == 'q') break;
+        }
+        System.out.println("Вы нажали q !");
+    }
+}
