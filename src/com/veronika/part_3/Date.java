e9951667ecb1d2c1f45c4519c483eff7c1c3b146
VasilevaVeronika <Sv670911@gmail.com>
2022-03-17 16:01:40 +0600

    Create Date.java

---
+++ src/com/veronika/part_3/Date.java
@@ -0,0 +1,10 @@
+package com.veronika.part_3;
+
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args) {
+        System.out.println("Привет,сегодня: ");
+        System.out.println(new java.util.Date());
+    }
+}
