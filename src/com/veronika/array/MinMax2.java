3c7426c8d736d069ef855d6e6fc2dd51d27ecf04
VasilevaVeronika <90505963+VasilevaVeronika@users.noreply.github.com>
2023-03-14 11:29:53 +0600

    Create MinMax2.java
    int nums[] = { 99, -10, 100123, 18, -978,
    5623, 4 63, -9, 287, 49 }
    так удобней

---
+++ src/com/veronika/array/MinMax2.java
@@ -0,0 +1,14 @@
+package com.veronika.array;
+
+public class MinMax2 {
+    public static void main(String[] args) {
+        int nums[] = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};//Инициализаторы массива
+        int min, max;
+        min = max = nums[0];
+        for (int i = 0; i < 10; i++) {
+            if (nums[i] < min) min = nums[i];
+            if (nums[i] > max) max = nums[i];
+        }
+        System.out.println("min и max" + min + " " + max);
+    }
+}
